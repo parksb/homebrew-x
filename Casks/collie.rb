433e97400b2b2836d1f2bc4db491b14df59d606d
parksb <parkgds@gmail.com>
2023-09-16 14:10:55 +0900

    collie: v0.1.6

--- Casks/collie.rb
+++ Casks/collie.rb
@@ -1,6 +1,6 @@
 cask "collie" do
-  version "0.1.5"
-  sha256 "6a1a0c18def3a79b42bdd1b41853426dbdbbe18516cc595ff530600831ff6bd4"
+  version "0.1.6"
+  sha256 "5063a35d4b170a8aa9e67df3af090f852ebc094f8f959baa068d067f9e2b748b"
 
   url "https://github.com/parksb/collie/releases/download/v#{version}/Collie_x64.app.tar.gz"
   name "Collie"
