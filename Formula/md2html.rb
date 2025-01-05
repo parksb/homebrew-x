4aa2b43770a85f16bd8819625ec2af75493d42e0
parksb <parkgds@gmail.com>
2025-01-05 18:38:28 +0900

    md2html: v0.0.9

--- Formula/md2html.rb
+++ Formula/md2html.rb
@@ -1,6 +1,6 @@
 class Md2html < Formula
-  version "0.0.8"
-  sha256 "cd39811d9913ff85190d6110e7c711815da1ef0fbd2988bb4a806efcd662e419"
+  version "0.0.9"
+  sha256 "4aa7f0da1309ffd6d1d87e8b15e5a8434fb6ac78a3355b88a9d192dfcdc5a1a6"
 
   url "https://github.com/parksb/md2html/releases/download/#{version}/md2html.tar.gz"
   desc "cat input.md | md2html --template github > output.html"
