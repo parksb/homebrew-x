387d85e38bf764b57635b95cd5f3254dc323e9f3
parksb <parkgds@gmail.com>
2025-05-22 22:12:49 +0900

    ised: v0.3.1

--- Formula/ised.rb
+++ Formula/ised.rb
@@ -1,17 +1,17 @@
 class Ised < Formula
-  version "0.3.0"
+  version "0.3.1"
   desc "An interactive tool for find-and-replace across many files"
   homepage "https://github.com/parksb/ised"
   license "AGPL-3.0-or-later"
 
   if Hardware::CPU.arm?
     url "https://github.com/parksb/ised/releases/download/v#{version}/ised-aarch64-apple-darwin.tar.gz"
-    sha256 "aace1ec1ee5923ab9ef5dc839d446cbf8fd656fd428e718d977eb07b233706be"
+    sha256 "9f4d0171cba22045df059dcd23ae7346ff93e67d20822694658fb934826a6c69"
   else
     url "https://github.com/parksb/ised/releases/download/v#{version}/ised-x86_64-apple-darwin.tar.gz"
-    sha256 "9a748df57ceec62c1764ed30593e77b56bbf22dfa634ef1eff35c7811226d5eb"
+    sha256 "0f1895bbbec123ae5cf9bd481d7689066f64cd99823a7812c0b59775dad2c904"
   end
-    
+
   def install
     bin.install "ised"
   end
